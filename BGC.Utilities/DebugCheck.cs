c9c9c5aed5a7dc9601db00f24d537039a86948af
zombunny <hselldf398@gmail.com>
2015-08-05 16:21:15 +0300

    Fixed missing uncommitted files (hooray, Github for Visual Studio). Improved formatting in Localization T4 template.

---
+++ BGC.Utilities/DebugCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGC.Utilities
+{
+    public static class DebugCheck
+    {
+		internal static readonly Func<Exception> DefaultExceptionProvider = () => new Exception("An unknown error occurred in the application");
+
+		public static void IsTrue(bool condition, Func<Exception> exceptionProvider = null)
+		{
+			if (!condition)
+			{
+				throw (exceptionProvider ?? DefaultExceptionProvider).Invoke();
+			}
+		}
+		public static void IsFalse(bool condition, Func<Exception> exceptionProvider = null)
+		{
+			IsTrue(!condition, exceptionProvider);
+		}
+    }
+}
---
+++ BGC.Utilities/Expressions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGC.Utilities
+{
+	public static class Expressions
+	{
+		private static Expression RemoveConvert(Expression expression)
+		{
+			// Expressions using constants use their values rather names after compilation. Therefore,
+			// any information regarding the constant's name is stripped from the Expression object.
+			DebugCheck.IsFalse(expression is ConstantExpression, () => new InvalidOperationException("Constant member access expressions are not supported."));
+
+			Expression result = expression;
+			while (
+				result.NodeType == ExpressionType.Convert ||
+				result.NodeType == ExpressionType.ConvertChecked)
+			{
+				result = ((UnaryExpression)result).Operand;
+			}
+
+			return result;
+		}
+
+		public static string NameOf<T>(Expression<Func<T>> memberExpression)
+		{
+			MemberExpression convertRemoved = RemoveConvert(memberExpression.Body) as MemberExpression;
+			string memberName = convertRemoved.Member.Name;
+			return memberName;
+		}
+
+		public static string NameOf<TObject>(Expression<Func<TObject, object>> memberExpression)
+		{
+			MemberExpression convertRemoved = RemoveConvert(memberExpression.Body) as MemberExpression;
+			string memberName = convertRemoved.Member.Name;
+			return memberName;
+		}
+	}
+}
