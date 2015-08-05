856d71b1673269686b885b45fd467d3e3040b308
zombunny <hselldf398@gmail.com>
2015-08-05 16:22:11 +0300

    One more missing file -.-

---
+++ BGC.Web/Areas/Administration/Filters/AdminAreaAuthorizationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BGC.WebAPI.Areas.Administration
+{
+	public class AdminAreaAuthorizationAttribute : AuthorizeAttribute
+	{
+		protected string GetActionUrl(RequestContext context, ActionResult action)
+		{
+			return new UrlHelper(context).ActionAbsolute(action);
+		}
+
+		protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+		{
+			string returnUrl = filterContext.RequestContext.HttpContext.Request.CurrentExecutionFilePath;
+			string loginUrl = this.GetActionUrl(filterContext.RequestContext, MVC.AdministrationArea.Authentication.Login());
+			
+			if (filterContext.Controller.TempData.ContainsKey(WebApiApplication.TempDataKeys.AdministrationArea.LoginSuccessReturnUrl))
+			{
+				filterContext.Controller.TempData.Remove(WebApiApplication.TempDataKeys.AdministrationArea.LoginSuccessReturnUrl);
+			}
+			
+			filterContext.Controller.TempData.Add(WebApiApplication.TempDataKeys.AdministrationArea.LoginSuccessReturnUrl, returnUrl);
+			filterContext.Result = new RedirectResult(loginUrl);
+		}
+	}
+}
