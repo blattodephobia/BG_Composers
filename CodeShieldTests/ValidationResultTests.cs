7ad47a5ea48c70545d9759935bb184364dac59d1
blattodephobia <hselldf398@gmail.com>
2016-08-09 01:25:18 +0300

    Ported CodeShieldTests to NUnit.

--- CodeShieldTests/ValidationResultTests.cs
+++ CodeShieldTests/ValidationResultTests.cs
@@ -1,5 +1,5 @@
 ﻿using CodeShield;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,87 +8,80 @@ using System.Threading.Tasks;
 
 namespace CodeShieldTests
 {
-    [TestClass]
-    public class ValidationTests
+    [TestFixture]
+    public class CtorTests
     {
-        [TestClass]
-        public class CtorTests
+        [Test]
+        public void SetsSuccessToTrueIfExceptionProviderIsNull()
         {
-            [TestMethod]
-            public void SetsSuccessToTrueIfExceptionProviderIsNull()
-            {
-                Validation<int> val = new Validation<int>(5, null);
-                Assert.IsTrue(val.Success);
-            }
-
-            [TestMethod]
-            public void SetsSuccessToFalseIfExceptionProviderIsNotNull()
-            {
-                Validation<int> val = new Validation<int>(5, x => new Exception());
-                Assert.IsFalse(val.Success);
-            }
+            Validation<int> val = new Validation<int>(5, null);
+            Assert.IsTrue(val.Success);
         }
 
-        [TestClass]
-        public class AndTests
+        [Test]
+        public void SetsSuccessToFalseIfExceptionProviderIsNotNull()
         {
-            [TestMethod]
-            public void SetsSuccessToTrueIfBothAreSucccessful()
-            {
-                Validation<int> val1 = new Validation<int>(3);
-                Validation<int> val2 = new Validation<int>(5);
-                Validation<int> result = val1.And(val2);
-                Assert.IsTrue(result.Success);
-            }
-
-            [TestMethod]
-            public void SetsSuccessToFalseIfEitherValidationFails1()
-            {
-                Validation<int> val1 = new Validation<int>(3, x => new Exception());
-                Validation<int> val2 = new Validation<int>(5);
-                Validation<int> result = val1.And(val2);
-                Assert.IsFalse(result.Success);
-
-            }
-
-            [TestMethod]
-            public void SetsSuccessToFalseIfEitherValidationFails2()
-            {
-                Validation<int> val1 = new Validation<int>(3);
-                Validation<int> val2 = new Validation<int>(5, x => new Exception());
-                Validation<int> result = val1.And(val2);
-                Assert.IsFalse(result.Success);
-            }
-
-            [TestMethod]
-            [ExpectedException(typeof(ArgumentException))]
-            public void ThrowsFirstValidationsExceptionIfItFails1()
-            {
-                Validation<int> val1 = new Validation<int>(3, x => new ArgumentException());
-                Validation<int> val2 = new Validation<int>(5, x => new IndexOutOfRangeException());
-                Validation<int> result = val1.And(val2);
-                result.ThrowOnError();
-            }
-
-            [TestMethod]
-            [ExpectedException(typeof(ArgumentException))]
-            public void ThrowsFirstValidationsExceptionIfItFails2()
-            {
-                Validation<int> val1 = new Validation<int>(3, x => new ArgumentException());
-                Validation<int> val2 = new Validation<int>(5);
-                Validation<int> result = val1.And(val2);
-                result.ThrowOnError();
-            }
-
-            [TestMethod]
-            [ExpectedException(typeof(IndexOutOfRangeException))]
-            public void ThrowsSecondValidationsExceptionIfOnlyItFails()
-            {
-                Validation<int> val1 = new Validation<int>(3);
-                Validation<int> val2 = new Validation<int>(5, x => new IndexOutOfRangeException());
-                Validation<int> result = val1.And(val2);
-                result.ThrowOnError();
-            }
+            Validation<int> val = new Validation<int>(5, x => new Exception());
+            Assert.IsFalse(val.Success);
         }
     }
+
+    [TestFixture]
+    public class AndTests
+    {
+        [Test]
+        public void SetsSuccessToTrueIfBothAreSucccessful()
+        {
+            Validation<int> val1 = new Validation<int>(3);
+            Validation<int> val2 = new Validation<int>(5);
+            Validation<int> result = val1.And(val2);
+            Assert.IsTrue(result.Success);
+        }
+
+        [Test]
+        public void SetsSuccessToFalseIfEitherValidationFails1()
+        {
+            Validation<int> val1 = new Validation<int>(3, x => new Exception());
+            Validation<int> val2 = new Validation<int>(5);
+            Validation<int> result = val1.And(val2);
+            Assert.IsFalse(result.Success);
+
+        }
+
+        [Test]
+        public void SetsSuccessToFalseIfEitherValidationFails2()
+        {
+            Validation<int> val1 = new Validation<int>(3);
+            Validation<int> val2 = new Validation<int>(5, x => new Exception());
+            Validation<int> result = val1.And(val2);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ThrowsFirstValidationsExceptionIfItFails1()
+        {
+            Validation<int> val1 = new Validation<int>(3, x => new ArgumentException());
+            Validation<int> val2 = new Validation<int>(5, x => new IndexOutOfRangeException());
+            Validation<int> result = val1.And(val2);
+            Assert.Throws<ArgumentException>(() => result.ThrowOnError());
+        }
+
+        [Test]
+        public void ThrowsFirstValidationsExceptionIfItFails2()
+        {
+            Validation<int> val1 = new Validation<int>(3, x => new ArgumentException());
+            Validation<int> val2 = new Validation<int>(5);
+            Validation<int> result = val1.And(val2);
+            Assert.Throws<ArgumentException>(() => result.ThrowOnError());
+        }
+
+        [Test]
+        public void ThrowsSecondValidationsExceptionIfOnlyItFails()
+        {
+            Validation<int> val1 = new Validation<int>(3);
+            Validation<int> val2 = new Validation<int>(5, x => new IndexOutOfRangeException());
+            Assert.Throws<IndexOutOfRangeException>(() => val1.And(val2).ThrowOnError());
+        }
+    }
+
 }
