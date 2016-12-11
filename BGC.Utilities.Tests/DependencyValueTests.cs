01339a8a3c8fad89c14a325449361f22ac12acf0
hselldf398 <hselldf398@gmail.com>
2016-12-11 20:12:03 +0200

    DependencyValue: EffectiveValue implementation.

--- BGC.Utilities.Tests/DependencyValueTests.cs
+++ BGC.Utilities.Tests/DependencyValueTests.cs
@@ -13,10 +13,14 @@ namespace BGC.Utilities.Tests
         private class DependencyValueProxy : DependencyValue<int>
         {
             [DependencyPrecedence(0)]
-            public DependencySource<int> Source1 { get; private set; }
+            public SingleValueDependencySource<int> Source1 { get; private set; }
 
             [DependencyPrecedence(1)]
             public DependencySource<int> Source2 { get; private set; }
+
+            public DependencySource<int> NoPrecedenceSource { get; private set; }
+
+            public DependencySource<bool> WrongSourceType { get; private set; }
 
             public IEnumerable<DependencySource<int>> GetDependencySourcesProxy() => GetDependencySources();
 
@@ -42,6 +46,52 @@ namespace BGC.Utilities.Tests
             Assert.IsTrue(sources[0] is MultiValueFifoDependencySource<int>);
             Assert.IsTrue(sources[1] is SingleValueDependencySource<int>);
             Assert.IsTrue(sources[2] is DefaultValueDependencySource<int>);
+        }
+    }
+
+    [TestFixture]
+    public class EffectiveValueTests
+    {
+        private class DependencyValueTestHelper : DependencyValue<int>
+        {
+            [DependencyPrecedence(0)]
+            public SingleValueDependencySource<int> Source1 { get; private set; }
+
+            public DependencyValueTestHelper() :
+                this(0)
+            {
+            }
+
+            public DependencyValueTestHelper(int defaultValue) :
+                base(defaultValue)
+            {
+                Source1 = new SingleValueDependencySource<int>();
+            }
+        }
+
+        [Test]
+        public void GetsHighestPriorityValueFirst()
+        {
+            DependencyValueTestHelper val = new DependencyValueTestHelper();
+            val.Source1.SetValue(23);
+            Assert.AreEqual(23, val.EffectiveValue);
+        }
+
+        [Test]
+        public void ReturnsDefaultValueWhenNoneIsSet()
+        {
+            DependencyValueTestHelper val = new DependencyValueTestHelper(17);
+            Assert.AreEqual(17, val.EffectiveValue);
+        }
+
+        [Test]
+        public void ReturnsDefaultValueWhenAllAreUnset()
+        {
+            DependencyValueTestHelper val = new DependencyValueTestHelper(17);
+            val.Source1.SetValue(2);
+            Assert.AreEqual(2, val.EffectiveValue);
+            val.Source1.UnsetValue();
+            Assert.AreEqual(17, val.EffectiveValue);
         }
     }
 }
--- BGC.Utilities/DependencyValue.cs
+++ BGC.Utilities/DependencyValue.cs
@@ -10,6 +10,10 @@ namespace BGC.Utilities
 {
     public abstract class DependencyValue<T>
     {
+        /// <summary>
+        /// Gets all properties of type <see cref="DependencySource{T}"/> or inheriting from it except the <see cref="DefaultValue"/> property. 
+        /// </summary>
+        /// <returns></returns>
         private Func<DependencyValue<T>, IEnumerable<DependencySource<T>>> GenerateDependencySourcesGetter()
         {
             Type currentType = GetType();
@@ -18,7 +22,6 @@ namespace BGC.Utilities
                                              where typeof(DependencySource<T>).IsAssignableFrom(property.PropertyType) && priorityAttribute != null
                                              orderby priorityAttribute.Priority descending
                                              select property).ToList();
-            properties.Add(currentType.GetProperty(nameof(DefaultValue), BindingFlags.Instance | BindingFlags.NonPublic));
             _dependencySourceGetter = Expressions.GetPropertyValuesOfTypeAccessor<DependencySource<T>>(currentType, properties);
             return _dependencySourceGetter;
         }
@@ -26,9 +29,49 @@ namespace BGC.Utilities
         private Func<DependencyValue<T>, IEnumerable<DependencySource<T>>> _dependencySourceGetter;
         private Func<DependencyValue<T>, IEnumerable<DependencySource<T>>> DependencySourcesGetter => _dependencySourceGetter ?? (_dependencySourceGetter = GenerateDependencySourcesGetter());
 
-        protected IEnumerable<DependencySource<T>> GetDependencySources() => DependencySourcesGetter.Invoke(this);
+        private List<DependencySource<T>> _sources;
+        private void SetEffectiveValue()
+        {
+            _effectiveValue = _sources.First(src => src.HasValue).GetEffectiveValue();
+
+        }
+
+        protected List<DependencySource<T>> GetDependencySources()
+        {
+            List<DependencySource<T>> result = new List<DependencySource<T>>(DependencySourcesGetter.Invoke(this));
+            result.Add(DefaultValue);
+            return result;
+        }
 
         protected DefaultValueDependencySource<T> DefaultValue { get; private set; }
+
+        protected void OnDependencySourceValueChanged(object sender, EffectiveValueChangedEventArgs<T> args)
+        {
+            SetEffectiveValue();
+        }
+
+        private T _effectiveValue;
+        public T EffectiveValue
+        {
+            get
+            {
+                if (_sources != null)
+                {
+                    return _effectiveValue;
+                }
+                else
+                {
+                    _sources = GetDependencySources().ToList();
+                    for (int i = 0; i < _sources.Count; i++)
+                    {
+                        _sources[i].EffectiveValueChanged += OnDependencySourceValueChanged;
+                    }
+                    SetEffectiveValue();
+                    return _effectiveValue;
+                }
+            }
+        }
+
 
         public DependencyValue()
         {
