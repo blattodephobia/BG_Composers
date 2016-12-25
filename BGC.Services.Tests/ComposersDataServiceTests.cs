c8b637969153ffd10c96547371ebb0ca5a9c8618
blattodephobia <hselldf398@gmail.com>
2016-12-25 02:08:38 +0200

    Implemented searching by composer name in the back end.

---
+++ BGC.Services.Tests/ComposersDataServiceTests.cs
@@ -0,0 +1,102 @@
+﻿using BGC.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TestUtils.Mocks;
+
+namespace BGC.Services.Tests
+{
+    [TestFixture]
+    public class SearchComposerNameTests
+    {
+        private static readonly List<Composer> ComposersRepo = new List<Composer>()
+        {
+            new Composer() { Id = 1, LocalizedNames = new List<ComposerName>()
+            {
+                new ComposerName(@"John Stamos", "en-US"),
+                new ComposerName(@"John Stamos", "de-DE"),
+                new ComposerName(@"Джон Стамос", "bg-BG"),
+            }},
+
+            new Composer() { Id = 2, LocalizedNames = new List<ComposerName>()
+            {
+                new ComposerName(@"Petar Stupel", "en-US"),
+                new ComposerName(@"Petar Stupel", "de-DE"),
+                new ComposerName(@"Петър Ступел", "bg-BG"),
+            }},
+
+            new Composer() { Id = 3, LocalizedNames = new List<ComposerName>()
+            {
+                new ComposerName(@"Pancho Vladigerov", "en-US"),
+                new ComposerName(@"Pancho Vladigerov", "de-DE"),
+                new ComposerName(@"Панчо Владигеров", "bg-BG"),
+            }},
+
+            new Composer() { Id = 4, LocalizedNames = new List<ComposerName>()
+            {
+                new ComposerName(@"John Atanasoff", "en-US"),
+                new ComposerName(@"John Atanasoff", "de-DE"),
+                new ComposerName(@"Джон Атанасов", "bg-BG"),
+            }},
+
+            new Composer() { Id = 5, LocalizedNames = new List<ComposerName>()
+            {
+                new ComposerName(@"Atanas Dalchev", "en-US"),
+                new ComposerName(@"Atanas Dalchev", "de-DE"),
+                new ComposerName(@"Атанас Далчев", "bg-BG"),
+            }},
+        };
+
+        [Test]
+        public void SearchComposer_SingleKeyword()
+        {
+            var svc = new ComposerDataService(GetMockRepository(ComposersRepo).Object, GetMockRepository(ComposersRepo.SelectMany(c => c.LocalizedNames).ToList()).Object);
+            IEnumerable<string> results = svc.Search("Atanas").ToList().Select(result => result.Header).Distinct();
+            
+            Assert.AreEqual(2, results.Count());
+            Assert.AreEqual("Atanas Dalchev", results.Single(header => header == "Atanas Dalchev"));
+            Assert.AreEqual("John Atanasoff", results.Single(header => header == "John Atanasoff"));
+        }
+
+        [Test]
+        public void SearchComposer_TwoKeywords()
+        {
+            var svc = new ComposerDataService(GetMockRepository(ComposersRepo).Object, GetMockRepository(ComposersRepo.SelectMany(c => c.LocalizedNames).ToList()).Object);
+            IEnumerable<string> results = svc.Search("Petar, Dalchev").ToList().Select(result => result.Header).Distinct();
+
+            Assert.AreEqual(2, results.Count());
+            Assert.AreEqual("Atanas Dalchev", results.Single(header => header == "Atanas Dalchev"));
+            Assert.AreEqual("Petar Stupel", results.Single(header => header == "Petar Stupel"));
+        }
+
+        [Test]
+        public void SearchComposer_TwoKeywords_SingleSearchPhrase()
+        {
+            var svc = new ComposerDataService(GetMockRepository(ComposersRepo).Object, GetMockRepository(ComposersRepo.SelectMany(c => c.LocalizedNames).ToList()).Object);
+            List<SearchResult> results = svc.Search("Petar Dalchev").ToList();
+
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [Test]
+        public void SearchComposer_ThreeKeywords_NoMatch()
+        {
+            var svc = new ComposerDataService(GetMockRepository(ComposersRepo).Object, GetMockRepository(ComposersRepo.SelectMany(c => c.LocalizedNames).ToList()).Object);
+            List<SearchResult> results = svc.Search("Ivan, Robert, Bach").ToList();
+
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [Test]
+        public void SearchComposer_ThreeKeywords_TwoSearchPhrases()
+        {
+            var svc = new ComposerDataService(GetMockRepository(ComposersRepo).Object, GetMockRepository(ComposersRepo.SelectMany(c => c.LocalizedNames).ToList()).Object);
+            List<SearchResult> results = svc.Search("Ivan, Панчо Владигеров").ToList();
+
+            Assert.AreEqual("Панчо Владигеров", results.First().Header);
+        }
+    }
+}
--- BGC.Services/ComposerDataService.cs
+++ BGC.Services/ComposerDataService.cs
@@ -10,35 +10,48 @@ using System.Globalization;
 
 namespace BGC.Services
 {
-	internal class ComposerDataService : DbServiceBase, IComposerDataService
-	{
-		protected IRepository<Composer> Composers { get; private set; }
+    internal class ComposerDataService : DbServiceBase, IComposerDataService, ISearchService
+    {
+        protected IRepository<Composer> Composers { get; private set; }
         protected IRepository<ComposerName> Names { get; private set; }
 
-		public ComposerDataService(IRepository<Composer> composers, IRepository<ComposerName> names)
-		{
+        private IEnumerable<SearchResult> SearchInternal(IEnumerable<string> keywords) =>
+            from resultName in (from composer in Composers.All()
+                                from name in composer.LocalizedNames
+                                where keywords.Any(keyword => name.FullName.Contains(keyword))
+                                select name).ToList()
+            select new SearchResult(resultName.Composer.Id)
+            {
+                Header = resultName.FullName,
+                ParsedResultXml = null
+            };
+
+        public ComposerDataService(IRepository<Composer> composers, IRepository<ComposerName> names)
+        {
             Shield.ArgumentNotNull(composers, nameof(composers)).ThrowOnError();
             Shield.ArgumentNotNull(names, nameof(names)).ThrowOnError();
-			this.Composers = composers;
-            this.Names = names;
-		}
 
-		public IList<Composer> GetAllComposers()
-		{
-			return this.Composers.All().ToList();
-		}
+            Composers = composers;
+            Names = names;
+        }
+
+        public IList<Composer> GetAllComposers() => Composers.All().ToList();
 
         public void Add(Composer composer)
         {
             Shield.ArgumentNotNull(composer, nameof(composer)).ThrowOnError();
 
-            this.Composers.Insert(composer);
+            Composers.Insert(composer);
             SaveAll();
         }
 
-        public IList<ComposerName> GetNames(CultureInfo culture)
-        {
-            return Names.All().Where(c => c.LanguageInternal == culture.Name).ToList();
-        }
+        public IList<ComposerName> GetNames(CultureInfo culture) => Names.All().Where(c => c.LanguageInternal == culture.Name).ToList();
+
+        public IEnumerable<SearchResult> Search(string query) =>
+            SearchInternal(
+                query
+                .ArgumentNotNull().GetValueOrThrow()
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Trim()));
     }
 }
