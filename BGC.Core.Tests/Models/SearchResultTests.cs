19d304b28f92343f517516ad1266d2d4dafcf900
blattodephobia <hselldf398@gmail.com>
2016-12-25 02:08:11 +0200

    Added searc results interface and data structure.

---
+++ BGC.Core.Tests/Models/SearchResultTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BGC.Core.Tests.Models
+{
+    [TestFixture]
+    public class SearchResult_CtorTests
+    {
+        [Test]
+        public void InitializesWithGuid()
+        {
+            var initialId = new Guid(4, 1, 1, new byte[8]);
+            var result = new SearchResult(initialId);
+            var currentId = new Guid(result.Id);
+            Assert.AreEqual(initialId, currentId);
+        }
+
+        [Test]
+        public void InitializesWithInt32_1()
+        {
+            var result = new SearchResult(4);
+            Assert.AreEqual(4, result.Id[0]);
+        }
+
+        [Test]
+        public void InitializesWithInt32_2()
+        {
+            var result = new SearchResult(0x0A0000BB);
+            Assert.AreEqual(0xBB, result.Id[0]);
+            Assert.AreEqual(0x0A, result.Id[3]);
+        }
+    }
+
+    [TestFixture]
+    public class SearchResult_XmlContentTests
+    {
+        [Test]
+        public void SetsParsedXmlProperty()
+        {
+            var result = new SearchResult();
+            Assert.IsNull(result.ParsedResultXml);
+
+            result.ResultXml = @"<root><child/></root>";
+            Assert.AreEqual("root", result.ParsedResultXml.DocumentElement.Name);
+            Assert.AreEqual("child", result.ParsedResultXml.DocumentElement.FirstChild.Name);
+        }
+
+        [Test]
+        public void ResetsParsedXmlProperty()
+        {
+            var result = new SearchResult();
+            Assert.IsNull(result.ParsedResultXml);
+
+            result.ResultXml = @"<root><child/></root>";
+            Assert.IsNotNull(result.ParsedResultXml);
+
+            result.ResultXml = null;
+            Assert.IsNull(result.ParsedResultXml);
+        }
+
+        [Test]
+        public void SetsXmlPropertyFromParsedXml()
+        {
+            var parsedXml = new XmlDocument();
+            parsedXml.LoadXml(@"<root><child/></root>");
+
+            var result = new SearchResult() { ParsedResultXml = parsedXml };
+            Assert.AreEqual(@"<root><child /></root>", result.ResultXml);
+        }
+
+        [Test]
+        public void ResetsXmlPropertyFromParsedXml()
+        {
+            var parsedXml = new XmlDocument();
+            parsedXml.LoadXml(@"<root><child/></root>");
+
+            var result = new SearchResult() { ParsedResultXml = parsedXml };
+            Assert.IsNotNull(result.ResultXml);
+
+            result.ParsedResultXml = null;
+            Assert.IsNull(result.ResultXml);
+        }
+    }
+}
---
+++ BGC.Core/Models/SearchResult.cs
@@ -0,0 +1,104 @@
+﻿using CodeShield;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace BGC.Core
+{
+    public class SearchResult
+    {
+        /// <summary>
+        /// An array that stores the ID of the entity or object that matched during in big-endian format.
+        /// </summary>
+        public byte[] Id { get; set; }
+
+        public string Header { get; set; }
+
+        private string _resultXml;
+        public string ResultXml
+        {
+            get
+            {
+                return _resultXml;
+            }
+
+            set
+            {
+                _resultXml = value;
+                _parsedResultXml = null;
+            }
+        }
+
+        private XmlDocument _parsedResultXml;
+        public XmlDocument ParsedResultXml
+        {
+            get
+            {
+                if (_parsedResultXml == null && ResultXml != null)
+                {
+                    _parsedResultXml = new XmlDocument();
+                    _parsedResultXml.LoadXml(ResultXml);
+                }
+
+                return _parsedResultXml;
+            }
+
+            set
+            {
+                _parsedResultXml = value;
+                _resultXml = _parsedResultXml?.OuterXml;
+            }
+        }
+
+        public SearchResult(byte[] id)
+        {
+            Shield.ArgumentNotNull(id);
+            Id = id;
+        }
+
+        public SearchResult()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SearchResult"/> instance and sets its <see cref="Id"/> to the bytes of the given integer.
+        /// </summary>
+        /// <param name="id"></param>
+        public SearchResult(int id)
+        {
+            Id = new byte[sizeof(int)];
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                Id[i] = (byte)((id >> (i * 8)) & 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SearchResult"/> instance and sets its <see cref="Id"/> to the bytes of the given 64-bit integer.
+        /// </summary>
+        /// <param name="id"></param>
+        public SearchResult(long id)
+        {
+            Id = new byte[sizeof(long)];
+            for (int i = 0; i < sizeof(long); i++)
+            {
+                Id[i] = (byte)((id >> (i * 8)) & 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SearchResult"/> instance and sets its <see cref="Id"/> to the bytes of the given <see cref="Guid"/>.
+        /// </summary>
+        /// <param name="id"></param>
+        public SearchResult(Guid id)
+        {
+            Id = id.ToByteArray();
+        }
+
+        public override string ToString() => Header;
+    }
+}
