81c48cc0fc69a40e1b302f8c14fddebb72c39ebb
blattodephobia <hselldf398@gmail.com>
2017-05-27 15:34:47 +0300

    Added authorization to PermissionsAttribute.

---
+++ BGC.Web.Tests/AdministrationArea/Filters/PermissionsAttributeTests.cs
@@ -0,0 +1,57 @@
+﻿using BGC.Core;
+using BGC.Web.Areas.Administration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGC.Web.Tests.AdministrationArea.Filters
+{
+    [TestFixture]
+    public class IsAuthorizedTests
+    {
+        [Test]
+        public void AuthorizesFullSetOfPermissions()
+        {
+            var attr = new PermissionsAttribute(typeof(UserSettingsPermission), typeof(SendInvitePermission));
+            Assert.IsTrue(attr.IsAuthorized(new IPermission[] { new UserSettingsPermission(), new SendInvitePermission() }));
+        }
+
+        [Test]
+        public void AuthorizesWithExtraPermissions()
+        {
+            var attr = new PermissionsAttribute(typeof(UserSettingsPermission), typeof(SendInvitePermission));
+            Assert.IsTrue(attr.IsAuthorized(new IPermission[] { new UserSettingsPermission(), new SendInvitePermission(), new ArticleManagementPermission() }));
+        }
+
+        [Test]
+        public void AuthorizesAnonymous()
+        {
+            var attr = new PermissionsAttribute();
+            Assert.IsTrue(attr.IsAuthorized(new IPermission[] { new UserSettingsPermission(), new SendInvitePermission(), new ArticleManagementPermission() }));
+        }
+
+        [Test]
+        public void NoAuthorizationForPartialSetOfPermissions()
+        {
+            var attr = new PermissionsAttribute(typeof(UserSettingsPermission), typeof(SendInvitePermission));
+            Assert.IsFalse(attr.IsAuthorized(new IPermission[] { new SendInvitePermission() }));
+        }
+
+        [Test]
+        public void ThrowsExceptionIfNoPermissions_1()
+        {
+            var attr = new PermissionsAttribute(typeof(UserSettingsPermission), typeof(SendInvitePermission));
+            Assert.Throws<InvalidOperationException>(() => attr.IsAuthorized(new IPermission[0]));
+        }
+
+        [Test]
+        public void ThrowsExceptionIfNoPermissions_2()
+        {
+            var attr = new PermissionsAttribute(typeof(UserSettingsPermission), typeof(SendInvitePermission));
+            Assert.Throws<InvalidOperationException>(() => attr.IsAuthorized(null));
+        }
+    }
+}
--- BGC.Web/Areas/Administration/Filters/PermissionsAttribute.cs
+++ BGC.Web/Areas/Administration/Filters/PermissionsAttribute.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using BGC.Core;
+using CodeShield;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace BGC.Web.Areas.Administration
 {
+    /// <summary>
+    /// Specifies the necessary permissions to perform an action. All of the required permissions have to be present for
+    /// this attribute to mark an action as allowed.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class PermissionsAttribute : Attribute
     {
@@ -13,6 +19,13 @@ namespace BGC.Web.Areas.Administration
         public PermissionsAttribute(params Type[] permissionTypes)
         {
             PermissionTypes = new HashSet<Type>(permissionTypes ?? Enumerable.Empty<Type>());
+        }
+
+        public bool IsAuthorized(IEnumerable<IPermission> assignedPermissions)
+        {
+            Shield.IsNotNullOrEmpty(assignedPermissions).ThrowOnError();
+
+            return assignedPermissions.Where(p => PermissionTypes.Contains(p.GetType())).Count() == PermissionTypes.Count;
         }
     }
 }
