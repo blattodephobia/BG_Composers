d5893795f2d954fa37b967348dcf126fcba681b8
hselldf398 <hselldf398@gmail.com>
2017-12-04 11:23:40 +0200

    Added strongly-typed model for global application settings.

---
+++ BGC.Core/Models/Settings/ApplicationConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGC.Core.Services;
+
+namespace BGC.Core
+{
+    public class ApplicationConfiguration : ConfigurationBase
+    {
+        public ApplicationConfiguration(ISettingsService settingsService) :
+            base(settingsService, "Global")
+        {
+        }
+
+        public string InvitationMessage
+        {
+            get
+            {
+                return ReadValue<string>();
+            }
+
+            set
+            {
+                SetValue(value);
+            }
+        }
+    }
+}
