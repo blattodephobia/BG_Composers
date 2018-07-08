579fa1520d09e7dc254e6c4f4233e756564f098b
ivann <hselldf398@gmail.com>
2018-07-08 18:16:00 +0300

    Clarified the expected behavior of IDtoFactory in its XML documentation.

--- BGC.Data/Relational/Mappings/IDtoFactory.cs
+++ BGC.Data/Relational/Mappings/IDtoFactory.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using BGC.Data.Relational.Mappings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BGC.Data.Relational.Mappings
+namespace BGC.Data.Relational
 {
     internal interface IDtoFactory
     {
-        TRelationalDto GetDtoFor<TRelationalDto, TEntity>(TEntity entity, RelationalPropertyMapper<TEntity, TRelationalDto> keyMapper) where TRelationalDto : RelationdalDtoBase;
+        /// <summary>
+        /// Returns an object of type <typeparamref name="TRelationalDto"/> corresponding to the <paramref name="entity"/>. The returned object may or may not have all of its properties set.
+        /// </summary>
+        /// <typeparam name="TRelationalDto"></typeparam>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entity">The entity whose DTO should be created or looked up. Can be null.</param>
+        /// <param name="mapper"></param>
+        /// <returns></returns>
+        TRelationalDto GetDtoFor<TRelationalDto, TEntity>(TEntity entity, RelationalPropertyMapper<TEntity, TRelationalDto> mapper) where TRelationalDto : RelationdalDtoBase;
     }
 }
